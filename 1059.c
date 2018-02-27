7aad577347742b03135261477392d073f666e02a
pat li <patli@qq.com>
2018-02-27 09:55:53 +0800

    add 1059.c

---
+++ 1059.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  char expr[250];
+
+  scanf("%s", expr);
+
+  char *p = NULL;
+  p = strtok(expr, "+");
+
+  int total = atoi(p);
+  while (p != NULL) {
+    p = strtok(NULL, "+");
+    if (p != NULL)
+      total += atoi(p);
+  }
+
+  printf("%d\n", total);
+  return 0;
+}
+
